ae58948830fc3e10068a259bd357042bd41122dd
xinnjie <xinnjie31@gmail.com>
2024-12-14 15:14:02 +0800

    fix lint

--- pkg/test/fake/results.go
+++ pkg/test/fake/results.go
@@ -32,7 +32,7 @@ func (c *ResultsClient) AddResult(name string, result *pb.Result) {
 }
 
 // GetResult implements ResultsClient.GetResult
-func (c *ResultsClient) GetResult(ctx context.Context, in *pb.GetResultRequest, opts ...grpc.CallOption) (*pb.Result, error) {
+func (c *ResultsClient) GetResult(_ context.Context, in *pb.GetResultRequest, _ ...grpc.CallOption) (*pb.Result, error) {
 	result, exists := c.results[in.Name]
 	if !exists {
 		return nil, fmt.Errorf("result not found: %s", in.Name)
@@ -41,7 +41,7 @@ func (c *ResultsClient) GetResult(ctx context.Context, in *pb.GetResultRequest, 
 }
 
 // ListResults implements ResultsClient.ListResults
-func (c *ResultsClient) ListResults(ctx context.Context, in *pb.ListResultsRequest, opts ...grpc.CallOption) (*pb.ListResultsResponse, error) {
+func (c *ResultsClient) ListResults(_ context.Context, _ *pb.ListResultsRequest, _ ...grpc.CallOption) (*pb.ListResultsResponse, error) {
 	results := make([]*pb.Result, 0, len(c.results))
 	for _, result := range c.results {
 		results = append(results, result)
@@ -53,46 +53,46 @@ func (c *ResultsClient) ListResults(ctx context.Context, in *pb.ListResultsReque
 }
 
 // CreateResult is unimplemented
-func (c *ResultsClient) CreateResult(ctx context.Context, in *pb.CreateResultRequest, opts ...grpc.CallOption) (*pb.Result, error) {
+func (c *ResultsClient) CreateResult(_ context.Context, _ *pb.CreateResultRequest, _ ...grpc.CallOption) (*pb.Result, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
 // UpdateResult is unimplemented
-func (c *ResultsClient) UpdateResult(ctx context.Context, in *pb.UpdateResultRequest, opts ...grpc.CallOption) (*pb.Result, error) {
+func (c *ResultsClient) UpdateResult(_ context.Context, _ *pb.UpdateResultRequest, _ ...grpc.CallOption) (*pb.Result, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
 // DeleteResult is unimplemented
-func (c *ResultsClient) DeleteResult(ctx context.Context, in *pb.DeleteResultRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+func (c *ResultsClient) DeleteResult(_ context.Context, _ *pb.DeleteResultRequest, _ ...grpc.CallOption) (*emptypb.Empty, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
 // CreateRecord is unimplemented
-func (c *ResultsClient) CreateRecord(ctx context.Context, in *pb.CreateRecordRequest, opts ...grpc.CallOption) (*pb.Record, error) {
+func (c *ResultsClient) CreateRecord(_ context.Context, _ *pb.CreateRecordRequest, _ ...grpc.CallOption) (*pb.Record, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
 // UpdateRecord is unimplemented
-func (c *ResultsClient) UpdateRecord(ctx context.Context, in *pb.UpdateRecordRequest, opts ...grpc.CallOption) (*pb.Record, error) {
+func (c *ResultsClient) UpdateRecord(_ context.Context, _ *pb.UpdateRecordRequest, _ ...grpc.CallOption) (*pb.Record, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
 // GetRecord is unimplemented
-func (c *ResultsClient) GetRecord(ctx context.Context, in *pb.GetRecordRequest, opts ...grpc.CallOption) (*pb.Record, error) {
+func (c *ResultsClient) GetRecord(_ context.Context, _ *pb.GetRecordRequest, _ ...grpc.CallOption) (*pb.Record, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
 // ListRecords is unimplemented
-func (c *ResultsClient) ListRecords(ctx context.Context, in *pb.ListRecordsRequest, opts ...grpc.CallOption) (*pb.ListRecordsResponse, error) {
+func (c *ResultsClient) ListRecords(_ context.Context, _ *pb.ListRecordsRequest, _ ...grpc.CallOption) (*pb.ListRecordsResponse, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
 // DeleteRecord is unimplemented
-func (c *ResultsClient) DeleteRecord(ctx context.Context, in *pb.DeleteRecordRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+func (c *ResultsClient) DeleteRecord(_ context.Context, _ *pb.DeleteRecordRequest, _ ...grpc.CallOption) (*emptypb.Empty, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
 // GetRecordListSummary is unimplemented
-func (c *ResultsClient) GetRecordListSummary(ctx context.Context, in *pb.RecordListSummaryRequest, opts ...grpc.CallOption) (*pb.RecordListSummary, error) {
+func (c *ResultsClient) GetRecordListSummary(_ context.Context, _ *pb.RecordListSummaryRequest, _ ...grpc.CallOption) (*pb.RecordListSummary, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
